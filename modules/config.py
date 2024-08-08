2f898b21196497f29aa99ba3ab724ca21e9a692c
Andrea Adelfio <and.adelfio@gmail.com>
2024-08-08 16:43:32 +0200

    updated modules/config.py with checks on unused variables

--- modules/config.py
+++ modules/config.py
@@ -1,37 +1,42 @@
 '''
-Configuration file for ACNBkg modules
+Configuration file containing the folders and filenames for ACNBkg modules.
+To check your configuration, run this file as a script.
 '''
 import os
-import pathlib
 
 USER = os.environ.get('USERNAME')
 
-DIR = pathlib.Path(os.path.dirname(os.path.realpath(__file__))).parent
-print(DIR)
+DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 # DIR = '/media/andrea/DISK4T1/ACD LAT Adelfio/'
 
 # Folders: parent (data), children (solar, spacecraft, LAT_ACD/output runs)
 DATA_FOLDER_NAME = os.path.join(DIR, 'data')
+
 SOLAR_FOLDER_NAME = os.path.join(DATA_FOLDER_NAME, 'solar')
+
 SC_FOLDER_NAME = os.path.join(DATA_FOLDER_NAME, 'spacecraft/LAT/weekly')
-DATA_LATACD_FOLDER_PATH = os.path.join(DATA_FOLDER_NAME, 'LAT_ACD/output runs')
+
+# DATA_LATACD_INPUT_FOLDER_PATH = os.path.join(DATA_FOLDER_NAME, 'LAT_ACD/Dec23-Feb24 input runs')
+DATA_LATACD_INPUT_FOLDER_PATH = os.path.join(DATA_FOLDER_NAME, 'LAT_ACD/May24-Jun24 input runs')
+
+# DATA_LATACD_FOLDER_PATH = os.path.join(DATA_FOLDER_NAME, 'LAT_ACD/Dec23-Feb24 output runs')
+DATA_LATACD_FOLDER_PATH = os.path.join(DATA_FOLDER_NAME, 'LAT_ACD/May24-Jun24 output runs')
+
 BACKGROUND_PREDICTION_FOLDER_NAME = os.path.join(DATA_FOLDER_NAME, 'background_prediction')
 
 # Folders: parent (data), children (solar, spacecraft, LAT_ACD/output runs)
 SOLAR_FILENAME = 'solar_activity'
 SOLAR_FILE_PATH = os.path.join(SOLAR_FOLDER_NAME, SOLAR_FILENAME)
 
-TILE_SIGNAL_FILENAME = 'tile_signal'
-TILE_SIGNAL_FILE_PATH = os.path.join(DATA_LATACD_FOLDER_PATH, TILE_SIGNAL_FILENAME)
+# TILE_SIGNAL_FILENAME = 'tile_signal'    # deprecated
+# TILE_SIGNAL_FILE_PATH = os.path.join(DATA_LATACD_FOLDER_PATH, TILE_SIGNAL_FILENAME)    # deprecated
 
-SC_FILENAME = 'sc'
-SC_FILE_PATH = os.path.join(SC_FOLDER_NAME, SC_FILENAME)
+# SC_FILENAME = 'sc'     # deprecated
+# SC_FILE_PATH = os.path.join(SC_FOLDER_NAME, SC_FILENAME)    # deprecated
 
 INPUTS_OUTPUTS_FILENAME = 'inputs_outputs'
 INPUTS_OUTPUTS_FOLDER = os.path.join(DATA_FOLDER_NAME, 'inputs_outputs')
 INPUTS_OUTPUTS_FILE_PATH = os.path.join(INPUTS_OUTPUTS_FOLDER, INPUTS_OUTPUTS_FILENAME)
-INPUTS_OUTPUTS_PK_FOLDER = os.path.join(DATA_FOLDER_NAME, 'inputs_outputs', 'pk')
-
 
 LOGGING_FOLDER_NAME = 'logs'
 LOGGING_FILE_NAME = f'acdbkg_{USER}.log'
@@ -39,3 +44,11 @@ LOGGING_FILE_REL_PATH = os.path.join(LOGGING_FOLDER_NAME, LOGGING_FILE_NAME)
 LOGGING_FILE_PATH = os.path.join(os.getcwd(), LOGGING_FILE_REL_PATH)
 
 MODEL_NN_SAVED_FILE_NAME = 'saved_model.keras'
+
+if __name__ == '__main__':
+    vars_copy = vars().copy()
+    filtered_vars = [var for var in vars_copy if '__' not in var]
+    max_len = max(len(var) for var in filtered_vars)
+
+    for var in filtered_vars:
+        print(f"{var:<{max_len}} {vars_copy[var]}")
